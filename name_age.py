3571cea7b2605390d15f8d9a100641e55ba5c311
gopal833 <106819438+gopal833@users.noreply.github.com>
2025-03-26 16:47:15 +1100

    Add files via upload

---
+++ name_age.py
@@ -0,0 +1,6 @@
+# Get the person's details
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Print the output combining the name and age
+print(f"Hello {name}, you are {age} years old.")
